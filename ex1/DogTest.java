14e385ac7cb143ca6b15420e06d26c7871ca42e2
Duncan Hall <duncan.hall@students.olin.edu>
2016-10-20 16:41:44 -0400

    done with exercise 1

---
+++ ex1/DogTest.java
@@ -0,0 +1,37 @@
+class Pet {
+    int size;
+    String breed;
+    String name;
+}
+
+class Dog extends Pet {
+    void bark() {
+        String template = "Imma %s lb dog. #bark";
+        System.out.println(String.format(template, size));
+    }
+}
+
+class Cat extends Pet {
+
+    public Cat () {
+        //no default parameter values in Java... );
+        name = "Bob";
+    }
+
+    void bark() {
+        String template = "lol, I'm %s the barking cat";
+        System.out.println(String.format(template, name));
+    }
+}
+
+public class DogTest {
+    public static void main (String[] args) {
+        Dog d = new Dog();
+        d.size = 40;
+        d.bark();
+
+        Cat c = new Cat();
+        c.breed = "Maine Coon";
+        c.bark();
+    }
+}
